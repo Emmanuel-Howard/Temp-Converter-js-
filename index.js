7c372b038d2c944d3de62676e01ade6413893187
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-24 09:02:19 -0500

    Pushing variables & constants

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Define constants, on click convert based on radio selection
+
+// Define constants & variables
+const tempBox = document.getElementById("tempBox");
+const fahrenheitRadio = document.getElementById("FahrenheitRadio");
+const celsiusRadio = document.getElementById("CelsiusRadio");
+const convertBtn = document.getElementById("convertBtn");
+let temp = document.getElementById("temp");
+let finalDegree = document.getElementById("finalDegree");

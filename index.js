d525f1ad0137256f96b93ca0d8eb37833bc2e3b9
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-24 09:07:49 -0500

    Initial conditional statement

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const celsiusRadio = document.getElementById("CelsiusRadio");
 const convertBtn = document.getElementById("convertBtn");
 let temp = document.getElementById("temp");
 let finalDegree = document.getElementById("finalDegree");
+
+// On click function
+convertBtn.onclick = function convert(){
+    if(fahrenheitRadio.checked){
+        console.log("F checked")
+    }
+    else if(celsiusRadio.checked){
+        console.log("C checked")
+    }
+    else{
+        console.log("Nothing is checked")
+    }
+}
